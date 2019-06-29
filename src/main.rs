c84b9c61bc2becfc5a9a470aaacfe57000e03fe8
Joe Frikker <jfrikker@gmail.com>
2019-06-29 14:37:44 -0400

    switching to walkdir

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,14 @@
-#[macro_use]
-extern crate quick_error;
-
 mod collectors;
-mod error;
-mod walker;
 
 use collectors::PerExtensionCount;
 use clap::{Arg, App};
-use error::Result;
-use nix::sys::stat::FileStat;
 use std::fmt::Debug;
 use std::fs::Metadata;
 use std::fs::File;
-use std::io::Read;
+use std::io::{BufRead, BufReader, Read, Result};
 use std::os::unix::io::{FromRawFd, RawFd};
 use std::path::Path;
-use walker::{Visitor, extension, walk};
+use walkdir::WalkDir;
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 enum CountType {
@@ -24,7 +17,7 @@ enum CountType {
     Lines
 }
 
-fn main() {
+fn main() -> Result<()> {
     let matches = App::new("codesize")
                           .version("1.0")
                           .author("Joe Frikker <jfrikker@gmail.com>")
@@ -55,69 +48,39 @@ fn main() {
     let human_readable = matches.is_present("h");
     let base_dir = matches.value_of("DIRECTORY").unwrap_or(".").to_owned();
 
-    let mut visitor = Counter {
-        counts: PerExtensionCount::new(),
-        count_type
-    };
-    walker::walk(&Path::new(&base_dir), &mut visitor).unwrap();
+    let mut counts = PerExtensionCount::new();
+
+    for entry in WalkDir::new(base_dir) {
+        let entry = entry?;
+        let ext = extension(entry.path());
+        let count = match count_type {
+            CountType::Files => 1,
+            CountType::Bytes => entry.metadata()?.len(),
+            CountType::Lines => count_lines(entry.path())?
+        };
+        counts.increment(ext, count);
+    }
 
     if !human_readable {
-        visitor.counts.print_counts(None);
+        counts.print_counts(None);
     } else if count_type == CountType::Bytes {
-        visitor.counts.print_counts(Some(1024));
+        counts.print_counts(Some(1024));
     } else {
-        visitor.counts.print_counts(Some(1000));
+        counts.print_counts(Some(1000));
     }
+
+    Ok(())
 }
 
-struct Counter {
-    counts: PerExtensionCount,
-    count_type: CountType
+fn count_lines(path: &Path) -> Result<u64> {
+    let file = File::open(path)?;
+    let file = BufReader::new(file);
+    Ok(file.lines().count() as u64)
 }
 
-impl Visitor for Counter {
-    fn enter_directory(&mut self, fd: RawFd, path: &Path) -> Result<bool> {
-        Ok(true)
-    }
-
-    fn leave_directory(&mut self) -> Result<()> {
-        Ok(())
-    }
-
-    fn visit(&mut self, fd: RawFd, path: &Path, stat: &FileStat) -> Result<()> {
-        let ext = walker::extension(path);
-        let count = match self.count_type {
-            CountType::Files =>  {
-                nix::unistd::close(fd)?;
-                1
-            },
-            CountType::Bytes => {
-                nix::unistd::close(fd)?;
-                stat.st_size as u64
-            },
-            CountType::Lines => count_lines(fd)?
-        };
-        self.counts.increment(ext, count);
-        Ok(())
-    }
-}
-
-fn count_lines(fd: RawFd) -> Result<u64> {
-    let mut file = unsafe { File::from_raw_fd(fd) };
-    let mut buf = [0;102400];
-    let mut result = 0;
-
-    #[allow(irrefutable_let_patterns)]
-    while let count = file.read(&mut buf)? {
-        if count == 0 {
-            break;
-        }
-
-        for c in buf[0..count].iter() {
-            if *c == 0x0A {
-                result += 1;
-            }
-        }
-    }
-    Ok(result)
+pub fn extension(path: &Path) -> String {
+    path.extension()
+        .and_then(|ext| ext.to_str())
+        .map(|ext| ext.to_string())
+        .unwrap_or(String::new())
 }
